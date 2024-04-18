a3dcfd5ca99bb04578b0d6a241b90b6cabd08195
roshankc <roshankc.py@gmail.com>
2024-04-18 18:11:32 +0545

    multiples_in_form_useState

--- complex_form/src/App.jsx
+++ complex_form/src/App.jsx
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 
 const App = () => {
   const [data, nextData] = useState({
-    email: "",
     fName: "",
     lName: "",
+    email: "",
+    phone: "",
   });
 
   const inputEvent = (received_data) => {
@@ -12,27 +13,36 @@ const App = () => {
     console.log(received_data.target.value);
     console.log(received_data.target.name);
 
-    const value = received_data.target.value;
-    const name = received_data.target.name;
+    // const value = received_data.target.value;
+    // const name = received_data.target.name;
+
+    const { value, name } = received_data.target;
 
     nextData((preValue) => {
-      if (name === "email") {
+      if (name === "fName") {
         return {
-          email: value,
-          fName: preValue.fName,
-          lName: preValue.lName,
-        };
-      } else if (name === "fName") {
-        return {
-          email: preValue.email,
           fName: value,
           lName: preValue.lName,
+          email: preValue.email,
         };
       } else if (name === "lName") {
         return {
-          email: preValue.email,
           fName: preValue.fName,
           lName: value,
+          email: preValue.email,
+        };
+      } else if (name === "email") {
+        return {
+          fName: preValue.fName,
+          lName: preValue.lName,
+          email: value,
+        };
+      } else if (name === "phone") {
+        return {
+          fName: preValue.fName,
+          lName: preValue.lName,
+          email: preValue.email,
+          phone: value,
         };
       }
     });
@@ -48,26 +58,34 @@ const App = () => {
       <form onSubmit={onSubmit}>
         <h1>{`Hello ${data.fName} ${data.lName}`}</h1>
         <h1>{`Your email is: ${data.email}`}</h1>
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          onChange={inputEvent}
-          value={data.email}
-        />
+        <h1>{`Your phone no: ${data.phone}`}</h1>
         <input
           type="text"
-          placeholder="first name"
+          placeholder="Enter your first name"
           name="fName"
           onChange={inputEvent}
           value={data.fName}
         />
         <input
           type="text"
-          placeholder="last name"
+          placeholder="Enter your last name"
           name="lName"
           onChange={inputEvent}
           value={data.lName}
+        />
+        <input
+          type="email"
+          placeholder="Enter your e-mail"
+          name="email"
+          onChange={inputEvent}
+          value={data.email}
+        />
+        <input
+          type="phone"
+          placeholder="Enter your phone number"
+          name="phone"
+          onChange={inputEvent}
+          value={data.phone}
         />
         <button type="submit">submit 👍</button>
       </form>
